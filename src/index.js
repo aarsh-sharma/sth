20e375d40a7c7c1429f93ef17b7aac68af0b84f2
Aarsh Sharma <aarsh01rsh@gmail.com>
2020-07-13 16:26:40 +0530

    modules added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import * as firebase from "firebase";
+import 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAJwSpXiOdCdT6Hl7kjWvONxI5aW14I-vw",
+  authDomain: "rich-text-notes.firebaseapp.com",
+  databaseURL: "https://rich-text-notes.firebaseio.com",
+  projectId: "rich-text-notes",
+  storageBucket: "rich-text-notes.appspot.com",
+  messagingSenderId: "339032380598",
+  appId: "1:339032380598:web:3f0ccb7a6b17c553fac217",
+  measurementId: "G-XCJZM99RYS"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
